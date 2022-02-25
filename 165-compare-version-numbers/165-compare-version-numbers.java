cbe4944547d7bd4173a0301622431abd8408ebf1
Anubhav Mishra <68318254+Anubhaavgit@users.noreply.github.com>
2022-02-25 12:40:08 +0530

    Time: 2 ms (54.25%), Space: 42.1 MB (20.07%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -3,20 +3,22 @@ class Solution {
          String[] arr1 = s1.split("\\.");
          String[] arr2 = s2.split("\\.");
         
-        int []arr3=new int[Math.max(arr1.length,arr2.length)];
+        int size=Math.max(arr1.length,arr2.length);
+        
+        int []arr3=new int[size];
         for(int i=0;i<arr1.length;i++)
         {
             arr3[i]=Integer.parseInt(arr1[i]);
         }
         
-        int []arr4=new int[Math.max(arr1.length,arr2.length)];
+        int []arr4=new int[size];
         for(int i=0;i<arr2.length;i++)
         {
             arr4[i]=Integer.parseInt(arr2[i]);
         }
         
         
-        for(int i=0;i<Math.max(arr1.length,arr2.length);i++)
+        for(int i=0;i<size;i++)
         {
             int a=arr3[i];
             int b=arr4[i];

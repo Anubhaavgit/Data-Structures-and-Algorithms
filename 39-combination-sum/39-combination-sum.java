548d836c6cc313efa67a994cc79a5e89d79e1aa0
Anubhav Mishra <68318254+Anubhaavgit@users.noreply.github.com>
2022-03-09 23:43:03 +0530

    Time: 3 ms (92.54%), Space: 44.7 MB (52.14%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,25 +1,24 @@
 class Solution {
-   void findCombination(int ind,int []arr,int target,List<List<Integer>>res,List<Integer>list)
-   {
-       if(ind==arr.length)
-       {
-           if(target==0)
-           {
-               res.add(new ArrayList<>(list));
-           }
-           return;
-       }
-       if(arr[ind]<=target)
-       {
-           list.add(arr[ind]);
-           findCombination(ind,arr,target-arr[ind],res,list);
-           list.remove(list.size()-1);
-       }
-       findCombination(ind+1,arr,target,res,list);
-   }
+    void findCombination(int index,int []candidates,int target,List<List<Integer>>ans,List<Integer>ds)
+    {
+        if(index==candidates.length)
+        {
+            if(target==0)
+                ans.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        if(candidates[index]<=target)
+        {
+            ds.add(candidates[index]);
+            findCombination(index,candidates,target-candidates[index],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+        findCombination(index+1,candidates,target,ans,ds);
+    }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>>res=new ArrayList<>();
-        findCombination(0,candidates,target,res,new ArrayList<>());
-        return res;
+        List<List<Integer>>ans=new ArrayList<>();
+        findCombination(0,candidates,target,ans,new ArrayList<>());
+        return ans;
     }
 }

d34943a308926991a23afc3bb97aa15c90a7866c
Anubhav Mishra <68318254+Anubhaavgit@users.noreply.github.com>
2022-03-12 23:46:06 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (7.05%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.java
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.java
@@ -2,33 +2,15 @@ class Solution {
     public int minCostToMoveChips(int[] position) {
         if(position.length==1)
             return 0;
-        if(position.length==2)
-        {
-            if(position[0]==position[1])
-                return 0;
-            return 1;
-        }
-        
-        
         int n=position.length;
-        int max=0;
+        int even=0,odd=0;
          for(int i=0;i<n;i++)
         {
-             if(position[i]>100)
-                 position[i]=position[i]%100;
-            max=Math.max(max,position[i]);
+             if(position[i]%2==0)
+                 even++;
+             else
+                 odd++;
         }
-        int []arr=new int[max+1];
-        for(int i=0;i<n;i++)
-        {
-            arr[position[i]]++;
-        }
-        for(int i=2;i<max+1;i++)
-        {
-            arr[i]=arr[i]+arr[i-2];
-        }
-        System.out.println(max);
-        System.out.println(Arrays.toString(arr));
-        return Math.min(arr[max-1],arr[max]);
+        return Math.min(odd,even);
     }
 }
